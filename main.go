983f3453916dcaf337dc96104423936d01f7d6d9
Matt Juskiw <juskiwmatt@gmail.com>
2020-11-20 13:43:55 -0500

    Added contact page

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my bad site</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my bad site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:juskiwmatt@gmail.com\">Matt Juskiw</a>.")
+	}
 }
 
 func main() {
